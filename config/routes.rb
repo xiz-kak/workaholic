8a5be33b8c93deae0bc312e48625c9b25a020bdf
xiz-tky <shizuka.kakimoto@linkage.jp.net>
2018-03-13 00:29:48 +0900

    make user_url accessible

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :create], format: 'json'
+  resources :users, only: [:index, :show, :create], format: 'json'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
